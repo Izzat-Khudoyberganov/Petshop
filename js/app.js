543fdcc56e07f40ddfa709ce356a2d2515547ca7
izzat <izzat@gmail.com>
2022-04-28 08:27:39 +0300

    custom dark mode

--- js/app.js
+++ js/app.js
@@ -1,48 +1,41 @@
+window.addEventListener('DOMContentLoaded', ()=>{
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-let menuBtn = document.querySelector('.header__menu'),
-nav = document.querySelector('.nav')
+  let menuBtn = document.querySelector('.header__menu'),
+    nav = document.querySelector('.nav')
 
 
-menuBtn.addEventListener('click', ()=> {
-  nav.classList.toggle('active')
-  menuBtn.classList.toggle('active')
+  menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+  })
+
+  const modal = document.querySelector('.modal__window'),
+    closeModal = document.querySelector('.modal__close-btn'),
+    body = document.querySelector('.header'),
+    card = document.querySelectorAll('.card'),
+    specialCards = document.querySelectorAll('.card__light')
+
+
+  function openModal() {
+    setTimeout(() => {
+      modal.style.display = 'flex'
+      modal.style.transition = '.5s'
+    }, 5000);
+  }
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none'
+  })
+  // openModal()
+
+  function mode() {
+    let request = prompt('Rang kiriting')
+    body.style.setProperty('--custom__bg', `${request}`)
+    card.forEach(element => {
+      element.style.setProperty('--custom__card', `${request}`)
+    });
+    specialCards.forEach(element => {
+      element.style.setProperty('--custom__card', `${request}`)
+    })
+  }
+  mode()
 })
-
-const modal = document.querySelector('.modal__window'),
-  closeModal = document.querySelector('.modal__close-btn'),
-  body = document.querySelector('body')
-
-
-function openModal(){
-  setTimeout(() => {
-    modal.style.display = 'flex'
-    modal.style.transition = '.5s'
-  }, 5000);
-}
-closeModal.addEventListener('click', ()=>{
-  modal.style.display = 'none'
-})
-openModal()
