1d235b85b9b46d3bf983308b8168bff2a9baaf7b
izzat <izzat@gmail.com>
2022-04-29 21:16:29 +0300

    add viewImg section's styles and functions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  let menuBtn = document.querySelector('.header__menu'),
-    nav = document.querySelector('.nav')
-
-
-  menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('active')
-    menuBtn.classList.toggle('active')
-  })
+  // Varaibles
 
   const modal = document.querySelector('.modal__window'),
     closeModal = document.querySelector('.modal__close-btn'),
@@ -15,10 +8,21 @@ window.addEventListener('DOMContentLoaded', () => {
     card = document.querySelectorAll('.card'),
     specialCards = document.querySelectorAll('.card__light'),
     customModeBtn = document.querySelector('.btn__custom-mode'),
-    upBtn = document.querySelector('.up__btn');
+    upBtn = document.querySelector('.up__btn'),
+    menuBtn = document.querySelector('.header__menu'),
+    nav = document.querySelector('.nav')
 
 
+  // Header menu -- Open//Close functions
 
+
+  menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+  })
+
+
+  // Modal Window -- Open//Close functions
 
   function openModal() {
     setTimeout(() => {
@@ -31,6 +35,8 @@ window.addEventListener('DOMContentLoaded', () => {
   })
   // openModal()
 
+
+  // Custom color mode -- Selecting color function  
   customModeBtn.addEventListener('click', () => {
     let request = prompt('Rang kiriting')
     header.style.setProperty('--headerBg', `${request}`)
@@ -43,7 +49,10 @@ window.addEventListener('DOMContentLoaded', () => {
   })
 
 
-  window.addEventListener('scroll', ()=>{
+  // ScrollTop function
+
+
+  window.addEventListener('scroll', () => {
     upBtn.classList.toggle('active', window.scrollY > 500)
   })
   upBtn.addEventListener('click', () => {
@@ -52,12 +61,54 @@ window.addEventListener('DOMContentLoaded', () => {
   })
 })
 
-// loader
 
-setTimeout(()=>{
+// Loader function 
+
+
+setTimeout(() => {
   let loader = document.querySelector('.loader');
   loader.style.opacity = '0';
-  setTimeout(()=>{
+  setTimeout(() => {
     loader.style.display = 'none'
   }, 2000)
 }, 3500)
+
+
+// OPP 
+class Img {
+  constructor(el) {
+    this.cards = document.querySelectorAll(el.cards)
+    this.view = document.querySelector(el.view)
+    this.closeBtn = document.querySelector(el.closeBtn)
+    this.viewImg = document.querySelector(el.viewImg)
+
+
+    const mainViewImg = this.viewImg
+    const viewArea = this.view
+    const closeViewBtn = this.closeBtn
+
+    closeViewBtn.addEventListener('click', () => {
+      viewArea.classList.toggle('active')
+    })
+    for (let i = 0; i < this.cards.length; i++) {
+      const element = this.cards[i];
+      element.addEventListener('dblclick', function () {
+        select(this)
+        viewArea.classList.add('active')
+      })
+    }
+    function select(img) {
+      let parentDiv = img.closest('.card');
+      let mainImg = parentDiv.querySelector('img');
+      let imgAtt = mainImg.getAttribute('src');
+      mainViewImg.setAttribute('src', `${imgAtt}`)
+    }
+  }
+}
+
+new Img({
+  cards: '.card',
+  view: '.view',
+  viewImg: '.view__img',
+  closeBtn: '.view__close'
+})
